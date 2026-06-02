ce32896e7e7b902be87e3f0d204306e62c3ebdc7
Felipe KiKo <felipe.kiko@gmail.com>
2026-06-02 13:30:33 -0300

    feat: sudeste: add organizers

--- src/regions/sudeste/index.tsx
+++ src/regions/sudeste/index.tsx
@@ -11,7 +11,7 @@ import flavioPhoto from "./assets/flavio-pimenta.jpg";
 import alexandrePhoto from "./assets/alexandre-ballestero-paula.jpg";
 import carlosPhoto from "./assets/carlos-augusto-souza-carvalho.jpg";
 import fabioPhoto from "./assets/fabio-baldin.png";
-import felipePhoto from "./assets/felipe-kiko.png";
+import felipePhoto from "./assets/felipe-kiko.jpg";
 import jessicaPhoto from "./assets/jessica-cristina-coelho.jpg";
 
 const photoMap: Record<string, string> = {
@@ -20,7 +20,7 @@ const photoMap: Record<string, string> = {
   "alexandre-ballestero-paula.jpg": alexandrePhoto,
   "carlos-augusto-souza-carvalho.jpg": carlosPhoto,
   "fabio-baldin.png": fabioPhoto,
-  "felipe-kiko.png": felipePhoto,
+  "felipe-kiko.jpg": felipePhoto,
   "jessica-cristina-coelho.jpg": jessicaPhoto,
 };
 
