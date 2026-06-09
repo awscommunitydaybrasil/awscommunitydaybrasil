7286bc68aa08329dbf4ecee511314d3042e2c219
Stephanie Ribeiro <sstephanie.ribeiro@gmail.com>
2026-06-09 14:12:42 -0300

    Atualizacao imagens organizers Sul

--- src/regions/sul/index.tsx
+++ src/regions/sul/index.tsx
@@ -6,10 +6,26 @@ import schedule from "./data/schedule.json";
 import sponsors from "./data/sponsors.json";
 import heroImage from "./assets/postcard-curitiba.png";
 
+import daianePhoto from "./assets/daiane.jpg";
+import lucasPhoto from "./assets/lucas.jpg";
+import leandroPhoto from "./assets/leandro.jpg";
+
+const photoMap: Record<string, string> = {
+  "daiane.jpg": daianePhoto,
+  "lucas.jpg": lucasPhoto,
+  "leandro.jpg": leandroPhoto,
+  
+};
+
+const resolvedOrganizers = organizers.map((org) => ({
+  ...org,
+  photo: photoMap[org.photo] || org.photo,
+}));
+
 const Sul = () => (
   <RegionPage
     config={config}
-    organizers={organizers}
+    organizers={resolvedOrganizers}
     speakers={speakers}
     schedule={schedule}
     sponsors={sponsors}
